ce1ef12fd4bf1589a7deb1be9b698f31b21ab41a
markkovgan67-ship-it <markkovgan67@gmail.com>
2025-09-23 00:05:35 +0300

    задание1

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, a, sum, mult;
+	cin >> n ;
+	cin >> a;
+	sum = 0;
+	for ( int i=0; i<=n ; i+=2)//четные чисал
+	{
+
+		sum = sum + i;
+		
+	
+    }
+	
+	mult = 1;
+	for (int i = 1; i <= a; i += 2)
+	{
+
+		 mult= mult * i;
+
+
+	}
+	cout << sum << endl;
+	cout << mult << endl;
+
+return 0;
+}
