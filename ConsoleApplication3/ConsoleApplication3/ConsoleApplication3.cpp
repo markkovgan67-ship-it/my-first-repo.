0fcd47dfadf1630a627f5636005f5c84463bb910
markkovgan67-ship-it <markkovgan67@gmail.com>
2025-09-23 00:07:05 +0300

    задание 3

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    int n;
+	cin >> n;
+    int a = 0, b = 1;
+    for (int i = 0; i < n; i++) {
+        cout << "F" << i << " = " << a << endl;
+        int next = a + b; // Fn = Fn-1 + Fn-2
+        a = b;
+        b = next;
+    }
+
+    return 0;
+
+}
+
